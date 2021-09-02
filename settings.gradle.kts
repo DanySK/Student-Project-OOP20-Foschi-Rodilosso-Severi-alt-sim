799bf946e74e1f8282f9686ab861cdf5fc7172a0
Roberto Casadei <robyonrails@gmail.com>
2021-09-02 11:44:04 +0200

    OOP machinery

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,4 +7,25 @@
  * in the user manual at https://docs.gradle.org/6.3/userguide/multi_project_builds.html
  */
 
+ import org.danilopianini.VersionAliases.justAdditionalAliases
+
+ plugins {
+     id("de.fayard.refreshVersions") version "0.10.1"
+ }
+
+ refreshVersions {
+     extraArtifactVersionKeyRules = justAdditionalAliases
+ }
+
+ buildscript {
+     repositories {
+         gradlePluginPortal()
+         mavenCentral()
+     }
+     dependencies {
+         classpath("org.danilopianini:refreshversions-aliases:+")
+     }
+ }
+
+
 rootProject.name = "OOP20-alt-sim"
